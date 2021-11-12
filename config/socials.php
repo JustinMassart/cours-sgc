a6c3cb6f146e1c4f01610f3fccb362840ba4d509
JustinMassart <justin.massart@student.hepl.be>
2021-11-12 10:42:53 +0100

    initial commit

---
+++ config/socials.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$socials = [
+    'facebook' => 'https://www.facebook.com/',
+    'twitter' => 'https://twitter.com/',
+    'gplus' => 'https://google.com/',
+    'pin' => 'https://www.pinterest.com/',
+    'youtube' => 'https://www.youtube.com/',
+];
+
