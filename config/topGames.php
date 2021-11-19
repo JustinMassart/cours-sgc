b70c769cacde20cc3d8c24a88ca9d95b5e700f7a
JustinMassart <justin.massart@student.hepl.be>
2021-11-19 10:03:36 +0100

    commit changes

---
+++ config/topGames.php
@@ -0,0 +1,34 @@
+<?php
+
+$topGames = [
+    [
+        'title' => 'Action Games',
+        'intro' => 'Nulla elementum nunc tempus.',
+        'img' => 'images/t1.jpg',
+    ],
+    [
+        'title' => 'Racing Games',
+        'intro' => 'Nulla elementum nunc tempus.',
+        'img' => 'images/t3.jpg',
+    ],
+    [
+        'title' => '3D Games Games',
+        'intro' => 'Nulla elementum nunc tempus.',
+        'img' => 'images/t4.jpg',
+    ],
+    [
+        'title' => 'Arcade Games',
+        'intro' => 'Nulla elementum nunc tempus.',
+        'img' => 'images/t2.jpg',
+    ],
+    [
+        'title' => 'Flight Games',
+        'intro' => 'Nulla elementum nunc tempus.',
+        'img' => 'images/t5.jpg',
+    ],
+    [
+        'title' => '2D Games',
+        'intro' => 'Nulla elementum nunc tempus.',
+        'img' => 'images/t6.jpg',
+    ],
+];
