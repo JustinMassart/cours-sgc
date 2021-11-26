2daa495e385aa465661deb7978911da149507349
JustinMassart <justin.massart@student.hepl.be>
2021-11-26 10:46:03 +0100

    commit changes - start of db

--- components/topGames.php
+++
@@ -1,8 +0,0 @@
-<article class="game-grid">
-    <h4><?= $title ?></h4>
-    <p><?= $excerpt ?></p>
-    <a href="<?= $url ?>">
-        <img src="<?= $img ?>" class="img-responsive"
-             alt="<?= $alt ?>" width="243" height="343"/>
-    </a>
-</article>
